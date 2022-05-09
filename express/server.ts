179cd600e79a529651d34faafbbacfbc818e9a21
MaxHam <45538872+MaxHam@users.noreply.github.com>
2022-05-09 16:17:27 +0200

    create express server

---
+++ express/server.ts
@@ -0,0 +1,24 @@
+const express = require("express");
+const basicAuth = require("express-basic-auth");
+const path = require("path");
+const app = express();
+
+app.use(
+	basicAuth({
+		challenge: true,
+		users: { cranach: "meisterwerk" },
+	})
+);
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+// add middlewares
+
+app.use((req, res, next) => {
+	res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+	console.log("server started on port 5000");
+});

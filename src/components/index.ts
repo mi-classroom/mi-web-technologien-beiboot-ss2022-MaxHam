2b96f12aaa3289ead5012fe59b4080c08d1de5e3
maxha <max.hammer1998@googlemail.com>
2022-08-16 19:37:46 +0200

    :art: funnel components into index file

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import CameraControls from './CameraControls';
+import Gallery from './Gallery';
+import Line from './Line';
+import PhyPlane from './PhyPlane';
+import Piece from './Piece';
+import Text from './Text';
+import Timeline from './Timeline';
+
+export { CameraControls, Gallery, Line, PhyPlane, Piece, Text, Timeline };
